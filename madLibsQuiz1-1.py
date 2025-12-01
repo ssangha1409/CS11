7faa7177ddad7e05bf2d31755e79b2dd3d1d5cda
ssangha1409 <supermansahib@icloud.com>
2025-11-30 22:43:18 -0800

    Update madLibsQuiz1-1.py

--- madLibsQuiz1-1.py
+++ madLibsQuiz1-1.py
@@ -8,21 +8,21 @@ print("|      Welcome to MadLibs          | ")
 print("|                                  |")
 print(************************************)
 
-(''MadLibs is a fill-in-the-blanks story
+print(''MadLibs is a fill-in-the-blanks story
 game. The player must choose words based on the 
 given prompts, and the computer will return a 
 short story that includes the words the user chose.''')
 
-play = onput("Do you want to play MadLibs?(y/n) ")
+play = input("Do you want to play MadLibs?(y/n) ")
 
-if play = "y"
+if play == "y":
     person_name = input("Choose a name for a person: ")
     place = input("Choose a place: ")
     noun_1 = input("Choose a singlular noun: ")
-    animal_1 = input(Choose an animal: )
+    animal_1 = input("Choose an animal: ")
     adjective_1 = input("Choose an adjective for a feeling: ")
     adjective_2 = input("Choose an adjective: ")
-    adjective_3  input("Choose an adjective: ")
+    adjective_3 = input("Choose an adjective: ")
     animal_2 = input("Choose an animal: ")
     food = input("Choose a food: ")
     
@@ -43,3 +43,4 @@ if play = "y"
     
 
     
+
